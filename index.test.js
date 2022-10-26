ed0c5500e121871ddd83b59962adfd8243ff69e9
Obed Vega <vega.obed@gmail.com>
2022-10-26 15:25:50 -0700

    fix base url

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 const request = require("supertest")
 //const baseURL = "http://localhost:5050"
-const baseURL = "https://app-demo-firebase.herokuapp.com/"
+const baseURL = "https://app-demo-firebase.herokuapp.com"
 const idx = require('./index');
 
 test('This is my demo project', async () => {
